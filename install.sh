dc6622be7d0552d53807cb8a0de600310cd82479
Robert Thompson <robert.thompson@aunalytics.com>
2013-04-17 14:46:24 -0400

    adding onto install script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+echo "Naming machine $1"
 echo "source \$HOME/Base/config/bash/bashrc" >> $HOME/.bashrc
 echo "source \$HOME/Base/config/bash/bash_aliases" >> $HOME/.bashrc
+echo "export PS1=[$1]\$PS1" >> $HOME/.bashrc
 echo "source \$HOME/Base/config/vim/vimrc" >> $HOME/.vimrc
 cp -r $HOME/Base/config/vim/vimfiles $HOME/.vim
+source $HOME/.bashrc
