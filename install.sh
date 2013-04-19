3ed1fe81c459e432dbb5c847fa01f531c2e5559b
root <root@debian-6.0.6-seed>
2013-04-19 15:37:44 +0000

    fixed vimfiles install

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ echo "source \$HOME/Base/config/bash/bashrc" >> $HOME/.bashrc
 echo "source \$HOME/Base/config/bash/bash_aliases" >> $HOME/.bashrc
 echo "export PS1=[$1]\$PS1" >> $HOME/.bashrc
 echo "source \$HOME/Base/config/vim/vimrc" >> $HOME/.vimrc
+rm -rf $HOME/.vim
 cp -r $HOME/Base/config/vim/vimfiles $HOME/.vim
 source $HOME/.bashrc
